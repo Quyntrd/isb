ceda2a1f6cfc1e259a64921346ce6d5f35873eb7
Quyntrd <daniil.terebilkib@gmail.com@git config --global user.name Quyntrdgit config --global user.email daniil.terebilkib@gmail.com@>
2024-05-19 20:38:20 +0400

    added two functions to file_readers

--- lab_3/file_readers.py
+++ lab_3/file_readers.py
@@ -7,13 +7,27 @@ def read_json_file(input_file: str):
     Parametres:
         input_file(str): path of the input file"""
     try:
-        with open(input_file, "r", encoding="utf-8") as f:
-            return json.load(f)
+        with open(input_file, "r", encoding="utf-8") as file:
+            return json.load(file)
     except Exception as exc:
         print(f"Failed to read json file: {exc}")
-    
 
-def write_file(output_file: str, output: list) -> None:
+
+def write_key(output_file: str, key: bytes) -> None:
+    """This function writes a key to a file
+    
+    Parametres:
+        output_file(str): path of the output file
+        
+        key(bytes): key to write"""
+    try:
+        with open(output_file, "wb") as file:
+            file.write(key)
+    except Exception as exc:
+        print(f"Failed to write to a file: {exc}")
+
+
+def write_file(output_file: str, output: str) -> None:
     """This function writes data into text file
     
     Parametres:
@@ -21,9 +35,25 @@ def write_file(output_file: str, output: list) -> None:
         
         output(str): text that needed to be written"""
     try:
-        with open(output_file, "w", encoding="utf-8") as o:
-            for out in output:
-                o.write(out)
-                o.write("\n")
+        if type(output) is bytes:
+            with open(output_file, "wb") as file:
+                file.write(output)
+        else:  
+            with open(output_file, "w") as file:
+                file.write(output)
     except Exception as exc:
         print(f"Failed to read txt file: {exc}")
+
+
+def read_txt(input_file: str):
+    """This function reads txt data from file
+    
+    Parametres:
+        input_file(str): path of the input file"""
+    try:
+        with open(input_file, "rb") as file:
+            data = file.read()
+        return data
+    except Exception as exc:
+        print(f"Failed reading file: {exc}")
+
---
+++ lab_3/main.py
@@ -0,0 +1,5 @@
+import argparse
+
+from asymetric import Asymmetric
+from symmetric import Symmetric
+from file_readers import read_json_file, write_file
